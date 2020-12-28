f5bb64c0db28debe512f0463629b4ea1e0c06bbe
Mateusz Kusiak (Timax) <timax2@wp.pl>
2020-12-28 16:21:44 +0100

    V.0.1.3
    Added screenshots

--- controllers/advertsController.js
+++ controllers/advertsController.js
@@ -116,11 +116,11 @@ exports.removeAdvert = (req, res) =>{
                         .then(response => {
                             if (response != null)
                                 res.status(200).json({
-                                    message: "Product id " + id + " removed succesfully",
+                                    message: "Advert id " + id + " removed succesfully",
                                     info: response
                                 })
                             else
-                                res.status(404).json({message: "No such product"})
+                                res.status(404).json({message: "No such advert"})
                         })
                         .catch( (err) =>{
                             res.status(500).json({message: err})
